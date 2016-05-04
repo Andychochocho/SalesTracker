9e2145d7f54118781db996f53a554d7e57cc0cb4
Andrew Cho and Matthew Newell <cho-andrew@hotmail.com>
2016-05-04 09:15:16 -0700

    add core files for user auth

---
+++ src/SalesTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesTracker.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
